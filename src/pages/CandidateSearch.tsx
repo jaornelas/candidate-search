6fbe1fa0f87312724e46d378e7df693c7e56f584
jaornelas <ornelalf001@gmail.com>
2025-03-02 22:56:59 -0600

    start candidateSearch

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,7 +1,34 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+import type {Candidate} from '../interfaces/Candidate.interface';
 
 const CandidateSearch = () => {
+const [results, setResults] = useState<Candidate[]>([]);
+const [currentUser, setCurrentUser] = useState<Candidate>({
+  id: null,
+    login: null,
+    email: null,
+    html_url: null,
+    name: null,
+    bio: null,
+    company: null,
+    location: null,
+    avatar_url: null,
+});
+const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+const searchForSpecUser = async (user: string) => {
+  const data: Candidate = await searchGithubUser(user);
+  setCurrentUser(data);
+}
+
+const searchForUsers = async () => {
+  const data: Candidate[] = await searchGithub();
+  setResults(data);
+
+  await searchForSpecUser(data[currentIndex].login || '');
+}
+
   return <h1>CandidateSearch</h1>;
 };
 
