79c18129e5d46c08adb03e2bb38baff1074f41d2
jaornelas <ornelalf001@gmail.com>
2025-03-19 22:16:55 -0500

    add structure to saved candidates

--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -1,61 +1,9 @@
-import type { Candidate } from '../interfaces/Candidate.interface';
-import { IoRemoveCircle } from 'react-icons/io5';
+import SavedCandidateList from '../components/SavedCandidateList';
 
-type SavedCandidatesProps = {
-  candidate: Candidate;
-  rejectCandidate: (id: number) => void;
-};
-
-const SavedCandidates = () => ({
-  candidate,
-  rejectCandidate,
-}: SavedCandidatesProps) => {
-
+const SavedCandidates = () => {
   return (
     <>
-      <td>
-            <img
-              src={`${candidate.avatar_url}`}
-              alt={`Profile of ${candidate.login}`}
-              style={{
-                width: '70px',
-                borderRadius: '10px',
-                display: 'block',
-                margin: '0 auto',
-              }}
-            />
-          </td>
-          <td>
-            <a href={candidate.html_url || ''} target='_blank' rel='noreferrer'>
-              <h3 style={{ color: 'white' }}>
-                {candidate.name}
-                <br />
-                <em>({candidate.login})</em>
-              </h3>
-            </a>
-          </td>
-          <td>{candidate.location}</td>
-          <td>
-            <a href={`mailto:${candidate.email}`}>{candidate.email}</a>
-          </td>
-          <td>{candidate.company}</td>
-          <td>
-            <div style={{ maxHeight: '100px', overflowY: 'scroll' }}>
-              {candidate.bio}
-            </div>
-          </td>
-          <td>
-            <IoRemoveCircle
-              style={{
-                color: 'red',
-                margin: '0 auto',
-                display: 'block',
-                cursor: 'pointer',
-                fontSize: '50px',
-              }}
-              onClick={() => rejectCandidate(candidate.id || 0)}
-            />
-          </td>
+      <h1>Potential Candidates</h1>
     </>
   );
 };
